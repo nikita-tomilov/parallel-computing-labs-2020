e09c36e374eccb926fae70608dd34db2e617305e
Nikita Tomilov <nikita.tomilov@siemens.com>
2020-09-14 09:06:25 +0300

    add results on pc

--- lab1/build-no-parallel.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -O3 -Wall -Werror -o lab1-seq lab1.c -lm
--- lab1/test-par.sh
+++ lab1/test-par.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+rm -rf *.txt
 echo "On single thread"
-./test-seq.sh
-for T in `seq 2 2 8`; do
+./test-seq.sh | tee logs-1.txt
+for T in `echo 2 4 6 8 12`; do
 	echo "Threads count:" $T
 	for N in `seq 2300 3470 37000`; do
-		./lab1-par-$T $N | grep passed
+		./lab1-par-$T $N | grep passed | tee -a logs-$T.txt
 	done
 done
+sed -i s/'. Milliseconds passed: '/','/g *.txt
+sed -i s/'N='/','/g *.txt
+pr -t -m logs-1.txt logs-2.txt logs-4.txt logs-6.txt logs-8.txt logs-12.txt | tee results.csv
+rm -rf *.txt
