a982b7658f9b9d1dc1d2544ffad590f54e6dba69
Nikita Tomilov <programmer174@icloud.com>
2020-09-19 15:29:21 +0300

    lab1: add results for clang & icc

--- lab1/test-par.sh
+++ lab1/test-par.sh
@@ -11,4 +11,4 @@ done
 sed -i s/'. Milliseconds passed: '/','/g *.txt
 sed -i s/'N='/','/g *.txt
 pr -t -m logs-1.txt logs-2.txt logs-4.txt logs-6.txt logs-8.txt logs-12.txt | tee results.csv
-rm -rf *.txt
+rm -rf logs-*
