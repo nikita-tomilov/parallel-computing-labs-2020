ec4dd8100d9b2cfd584a7a9bd020242c2f75418e
Nikita Tomilov <programmer174@icloud.com>
2020-11-22 22:30:40 +0300

    lab5.c: rewrite multiThreadComputing

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -73,140 +73,152 @@ void *printPercent(void *i) {
     return 0;
 }
 
-struct arg_struct {//структура которую распаралеливаемая вункция получает как параметры
-    double* array;
-	long arrayLenght;
-} ;
+typedef struct {
+    double *array;
+    long offset;
+    long count;
+} Chunk_t;
 
-void voidFunc(){};
+typedef struct {
+    int threadId;
+    void *func;
+} ThreadInfo_t;
 
-pthread_t* firstFreedThread(pthread_t *threads, int threadsNum){
-	while(1){
-		for(int i = 0 ; i < threadsNum ; i++){
-			//printf("hear 1\n");
-			if( pthread_tryjoin_np(threads[i], NULL) != 0){
-				//printf("hear 2\n");
-				return (pthread_t*)threads[i];
-			}
-		}	
-	}
-};
+pthread_mutex_t mutex;
+double *arrayForProcessing;
+long arrayLength;
+long currentOffset;
+long chunkSize;
 
-void multiThreadComputing(int threadsNum, double array[], long arrayLenght, void* func, void* funcReduction, int chunkSize){
-	pthread_t *thread = (pthread_t *) malloc(threadsNum * sizeof(pthread_t));//создаем потоки
-	int chunks = arrayLenght / chunkSize;//вычисляем кол-во кусков
-	chunks -= 1;//последний кусок может быть меньше указаного размера
-	struct arg_struct* argsArray = malloc(sizeof(struct arg_struct) * chunks);
-	for(int j = 0; j < threadsNum; j++){//запускаем пустые функции
-		pthread_create(&thread[j], NULL, (void*)voidFunc, NULL);
-	}
-
-	for(int i = 0 ; i < chunks ; i++){ //работаем куски размером chunkSize
-		argsArray[i].arrayLenght = chunkSize;
-		argsArray[i].array = &array[i * chunkSize];
-		printf("chunk %d address %p\n", i, (void*)argsArray[i].array);
-		pthread_t *threadFreed = firstFreedThread(thread, threadsNum);
-		pthread_create(threadFreed, NULL, func, &argsArray[i]);
-	}
-	//считаем огрызок если есть
-	if(arrayLenght % chunkSize > 0){
-		struct arg_struct* stubArgs = malloc(sizeof(struct arg_struct));
-		stubArgs->arrayLenght = arrayLenght % chunkSize;
-		stubArgs->array = &array[chunks * chunkSize];
-		pthread_t *threadFreed = firstFreedThread(thread, threadsNum);
-		pthread_create(threadFreed, NULL, (void*)func, stubArgs);
-		pthread_join(*threadFreed, NULL);
-		free(stubArgs);
-	}
-
-	for(int i = 0 ; i < threadsNum ; i++){
-		pthread_join(thread[i], NULL);
-	}
-	free(thread);
-	free(argsArray);
+Chunk_t getNextChunk(int threadId) {
+    pthread_mutex_lock(&mutex);
+    Chunk_t ans;
+    ans.array = arrayForProcessing;
+    if (currentOffset >= arrayLength) {
+        ans.count = 0;
+        ans.offset = 0;
+    } else {
+        ans.offset = currentOffset;
+        ans.count = MIN(chunkSize, arrayLength - currentOffset);
+    }
+    currentOffset = currentOffset + chunkSize;
+    printf("thread %d requested next chunk, i said from %ld with size %ld\n", threadId, ans.offset, ans.count);
+    pthread_mutex_unlock(&mutex);
+    return ans;
 }
 
-void m1Map(struct arg_struct *args){
-	for (int j = 0; j < args->arrayLenght; j++) {//Кубический корень после деления на число e
-		//printf("%d\n",(*((double*)args->array)));
-		args->array[j] = pow((double) (args->array[j] / exp(1.0)), 1.0 / 3.0);
-	}
+void threadFunc(void *args) {
+    ThreadInfo_t info = *(ThreadInfo_t *) args;
+    Chunk_t chunk = getNextChunk(info.threadId);
+    void (*f)(Chunk_t);
+    f = info.func;
+    while (chunk.count > 0) {
+        f(chunk);
+        chunk = getNextChunk(info.threadId);
+    }
 }
-void m2Map(struct arg_struct *args){
-	for (int j = 0; j < args->arrayLenght; j++) {//Десятичный логарифм, возведенный в степень e
-			double sum = args->array[j]; //для нач элемента массива предыдущий элемент равен0
-			if (j > 0) {
-				sum += args->array[j - 1]; //каждый элемент поочерёдно сложить с предыдущим
-			}
-			args->array[j] = pow(log10(sum), exp(1.0));
-		}
+
+void multiThreadComputing(double _array[], long _arrayLength, int threadsNum, void *func, void *funcReduction,
+                          int _chunkSize) {
+    pthread_t *thread = (pthread_t *) malloc(threadsNum * sizeof(pthread_t)); //создаем потоки
+    ThreadInfo_t *info = (ThreadInfo_t *) malloc(threadsNum * sizeof(ThreadInfo_t)); //создаем инфо
+
+    arrayForProcessing = _array;
+    arrayLength = _arrayLength;
+    chunkSize = _chunkSize;
+
+    for (int j = 0; j < threadsNum; j++) {
+        info[j].threadId = j + 1;
+        info[j].func = func;
+        pthread_create(&thread[j], NULL, (void *) threadFunc, (void *) (info + j));
+    }
+
+    for (int i = 0; i < threadsNum; i++) {
+        pthread_join(thread[i], NULL);
+    }
+    free(thread);
+    free(info);
+}
+
+void m1Map(Chunk_t chunk) {
+    double *array = chunk.array;
+    long N = chunk.offset + chunk.count;
+    printf("m1map over %p from %ld len %ld\n", array, chunk.offset, chunk.count);
+    for (long j = chunk.offset; j < N; j++) {//Кубический корень после деления на число e
+        array[j] = pow((double) (array[j] / exp(1.0)), 1.0 / 3.0);
+    }
 }
 
 int main(int argc, char *argv[]) {
     int i = 0;
     long delta_ms;
     int N;
-	
-	struct timeval T1, T2;
-	gettimeofday(&T1, NULL); /* запомнить текущее время T1 */
-	pthread_t thread;
-	pthread_create(&thread, NULL, printPercent, &i);
+
+    struct timeval T1, T2;
+    gettimeofday(&T1, NULL); /* запомнить текущее время T1 */
+    pthread_t thread;
+    pthread_create(&thread, NULL, printPercent, &i);
 
 
-	int j;
-	N = atoi(argv[1]); /* N равен первому параметру командной строки */
-	int M2_size = N / 2;
-	double *M1 = (double *) malloc(N * sizeof(double));
-	printf("address M1 %p\n",(void*)M1);
-	double *M2 = (double *) malloc(M2_size * sizeof(double));
-	double *M2_copy = (double *) malloc((M2_size) * sizeof(double));
-	unsigned int A = (unsigned int) 240;
+    int j;
+    N = atoi(argv[1]); /* N равен первому параметру командной строки */
+    int M2_size = N / 2;
+    double *M1 = (double *) malloc(N * sizeof(double));
+    printf("address M1 %p\n", (void *) M1);
+    double *M2 = (double *) malloc(M2_size * sizeof(double));
+    double *M2_copy = (double *) malloc((M2_size) * sizeof(double));
+    unsigned int A = (unsigned int) 240;
 
 
-	for (i = 0; i < MAX_I; i++) /* 50 экспериментов */
-	{
-		/* Заполнить массив исходных данных размером N */
-		//aka Этап Generate
-		unsigned int seed = i;
-		for (j = 0; j < N; j++) {// генерим М1
-			M1[j] = custom_rand(1.0, A, &seed);
-		}
-
-		for (j = 0; j < M2_size; j++) {// генерим М2 и его копию
-			double rand = custom_rand(A, 10.0 * A, &seed);
-			M2[j] = rand;
-			M2_copy[j] = rand;
-		}
-		
-		//multiThreadComputing(2, M1, N, printHello, NULL, 20);
-
-		/* Решить поставленную задачу, заполнить массив с результатами */
-		//aka этап Map для M1
-		//m1Map(M1);
-		multiThreadComputing(2, M1, N, m1Map, NULL, 20);
-		
-		printf("print M1\n");
-		for (j = 0; j < N; j++) {//Кубический корень после деления на число e
-					printf("%d - %f\n",j, M1[j]);
+    for (i = 0; i < MAX_I; i++) /* 50 экспериментов */
+    {
+        /* Заполнить массив исходных данных размером N */
+        //aka Этап Generate
+        unsigned int seed = i;
+        for (j = 0; j < N; j++) {// генерим М1
+            M1[j] = custom_rand(1.0, A, &seed);
         }
 
-		//этап Map для M2
-		//multiThreadComputing(1, M2_copy, M2_size, m2Map, NULL, 20);
-		/*for (j = 0; j < M2_size; j++) {//Десятичный логарифм, возведенный в степень e
-			double sum = M2_copy[j]; //для нач элемента массива предыдущий элемент равен0
-			if (j > 0) {
-				sum += M2_copy[j - 1]; //каждый элемент поочерёдно сложить с предыдущим
-			}
-			M2[j] = pow(log10(sum), exp(1.0));
-		}*/
+        for (j = 0; j < M2_size; j++) {// генерим М2 и его копию
+            double rand = custom_rand(A, 10.0 * A, &seed);
+            M2[j] = rand;
+            M2_copy[j] = rand;
+        }
 
-		//этап Merge
-		for (j = 0; j < M2_size; j++) {//Модуль разности
-			M2[j] = fabs(M1[j] - M2[j]);
-		}
+        //multiThreadComputing(2, M1, N, printHello, NULL, 20);
 
-		/* Отсортировать массив с результатами указанным методом */
-		//aka этап Sort
+        /* Решить поставленную задачу, заполнить массив с результатами */
+        //aka этап Map для M1
+
+        multiThreadComputing(M1, N, 2, m1Map, NULL, 20);
+        /*Chunk_t chunk;
+        chunk.array = M1;
+        chunk.count = N;
+        chunk.offset = 0;
+        m1Map(chunk);*/
+
+        printf("print M1\n");
+        for (j = 0; j < N; j++) {//Кубический корень после деления на число e
+            printf("%d - %f\n", j, M1[j]);
+        }
+
+        //этап Map для M2
+        //multiThreadComputing(1, M2_copy, M2_size, m2Map, NULL, 20);
+        /*for (j = 0; j < M2_size; j++) {//Десятичный логарифм, возведенный в степень e
+            double sum = M2_copy[j]; //для нач элемента массива предыдущий элемент равен0
+            if (j > 0) {
+                sum += M2_copy[j - 1]; //каждый элемент поочерёдно сложить с предыдущим
+            }
+            M2[j] = pow(log10(sum), exp(1.0));
+        }*/
+
+        //этап Merge
+        for (j = 0; j < M2_size; j++) {//Модуль разности
+            M2[j] = fabs(M1[j] - M2[j]);
+        }
+
+        /* Отсортировать массив с результатами указанным методом */
+        //aka этап Sort
 
 
 /*#pragma omp parallel default(none) shared(M2, M2_size) num_threads(k)
@@ -232,40 +244,40 @@ int main(int argc, char *argv[]) {
 		//но сам массив еще нет. когда у нас массив частично сортирован, mergeSort - то что нужно
 		mergeSort(M2, 0, M2_size - 1);
 */
-		
-		long location;
-                double elem;
-                for (j = 1; j < M2_size; j++) {//Сортировка вставками (Insertion sort).
-                    elem = M2[j];
-                    location = j - 1;
-                    while (location >= 0 && M2[location] > elem) {
-                        M2[location + 1] = M2[location];
-                        location = location - 1;
-                    }
-                    M2[location + 1] = elem;
-                }
+
+        long location;
+        double elem;
+        for (j = 1; j < M2_size; j++) {//Сортировка вставками (Insertion sort).
+            elem = M2[j];
+            location = j - 1;
+            while (location >= 0 && M2[location] > elem) {
+                M2[location + 1] = M2[location];
+                location = location - 1;
+            }
+            M2[location + 1] = elem;
+        }
 
 
-		double minNotZero = 0;
+        double minNotZero = 0;
 //#pragma omp parallel for default(none) shared(M2, M2_size, i) reduction(min:minNotZero) num_threads(CONST_NUM_THREADS) SCHEDULE_STRING
-		for (j = 0; j < M2_size; j++) {//ищим минимальный ненулевой элемент массива М2
-			if (M2[j] > 0 && M2[j] < minNotZero)
-				minNotZero = M2[j];
-		}
+        for (j = 0; j < M2_size; j++) {//ищим минимальный ненулевой элемент массива М2
+            if (M2[j] > 0 && M2[j] < minNotZero)
+                minNotZero = M2[j];
+        }
 
-		double X = 0;
+        double X = 0;
 //#pragma omp parallel for default(none) shared(M2, M2_size, minNotZero, i) reduction(+:X) num_threads(CONST_NUM_THREADS) SCHEDULE_STRING
-		for (j = 0; j < M2_size; j++) {//Рассчитать сумму синусов тех элементов массиваМ2,
-			//которые при делении на минимальный ненулевой
-			//элемент массива М2 дают чётное число
-			if (((long) (M2[j] / minNotZero)) % 2 == 0)
-				X += sin(M2[i]);
-		}
-		//printf("N=%d\t X=%.20f\n", i, X);
-	}
-	free(M1);
-	free(M2);
-	free(M2_copy);
+        for (j = 0; j < M2_size; j++) {//Рассчитать сумму синусов тех элементов массиваМ2,
+            //которые при делении на минимальный ненулевой
+            //элемент массива М2 дают чётное число
+            if (((long) (M2[j] / minNotZero)) % 2 == 0)
+                X += sin(M2[i]);
+        }
+        //printf("N=%d\t X=%.20f\n", i, X);
+    }
+    free(M1);
+    free(M2);
+    free(M2_copy);
 
     gettimeofday(&T2, NULL);   /* запомнить текущее время T2 */
     delta_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec) / 1000;
