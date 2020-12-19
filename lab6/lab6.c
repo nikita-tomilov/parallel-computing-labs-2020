a1fc2648c9df28b24284c1c666e9f68e37ce621b
ilya <ilyapn@outlook.com>
2020-12-19 17:47:51 +0300

    complete

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -24,16 +24,47 @@ cl_uint ret;
 cl_device_id device;
 cl_uint uNumCPU;
 
-size_t source_size;
-char *source_str;
-const int g_cuNumItems = 600;
+void createProgram(char *fileName, char *source_str, size_t source_size);
+void createProgram_sin(char *fileName);
+
+cl_mem M2_clmem_sin;
+cl_mem M2_ret_clmem_sin;
+cl_kernel kernel_sin;
+cl_command_queue queue_sin;
+cl_context context_sin;
+cl_float *puData_sin;
+cl_program program_sin;
+char *source_str_sin;
+size_t source_size_sin;
+char *source_str_sin;
+size_t source_size_sin;
+
+cl_mem M1_clmem_mrg;
+cl_mem M1_ret_clmem_mrg;
+cl_mem M2_clmem_mrg;
+cl_mem M2_ret_clmem_mrg;
+cl_kernel kernel_mrg;
+cl_command_queue queue_mrg;
+cl_context context_mrg;
+cl_float *puData_mrg;
+cl_program program_mrg;
+char *source_str_mrg;
+size_t source_size_mrg;
+char *source_str_mrg;
+size_t source_size_mrg;
+
 //const int num_cores = CL_DEVICE_MAX_COMPUTE_UNITS;
 const int num_cores = 12;
 
-void mergecl(float *M1, float *M2, int M2_size);
+void cl_sin_init(int M2_size);
+
+void cl_sin_destroy();
 
 float sum_sin(float *M2, int M2_size, float min_non_zero);
 
+void mergecl(float *M1, float *M2, int M2_size);
+void cl_mrg_init(int M2_size);
+void cl_mrg_destroy();
 
 int NUM_THREADS = 4;
 int CHUNK_SIZE = 20;
@@ -342,6 +373,9 @@ int main(int argc, char *argv[]) {
     float *M2_copy = (float *) malloc((M2_size) * sizeof(float));
     unsigned int A = (unsigned int) 240;
 
+    cl_sin_init(M2_size);//openCL init
+    cl_mrg_init(M2_size);
+
     if (argc > 2) {
         NUM_THREADS = (int) strtol(argv[2], &end, 10);
         if (NUM_THREADS == 0) {
@@ -387,13 +421,13 @@ int main(int argc, char *argv[]) {
         delta_map_stage_us += get_time_us();
 
         //этап Merge
-/*        if (WORK_PARALLEL) {
+        /*if (WORK_PARALLEL) {
             multiThreadComputing(M2, M1, M2_size, NUM_THREADS, Merge, CHUNK_SIZE);
         } else {
             Merge(fullChunk(M2, M1, M2_size));
         }*/
         //OpenCL
-        mergecl(M1,M2, M2_size);
+        mergecl(M1, M2, M2_size);
         delta_merge_stage_us += get_time_us();
 
         /* Отсортировать массив с результатами указанным методом */
@@ -426,7 +460,7 @@ int main(int argc, char *argv[]) {
           элемент массива М2 дают чётное число*/
         //aka Reduce-2
         float X;
-        /*if (WORK_PARALLEL) {
+/*        if (WORK_PARALLEL) {
             multiThreadComputingReduction(M2, &minNotZero, M2_size, NUM_THREADS, SumOfSine, reductionSum, 0,
                                           CHUNK_SIZE);
             X = reductionResult;
@@ -456,11 +490,12 @@ int main(int argc, char *argv[]) {
     printf("Reduce:%ld\n", delta_reduce_stage_us);
 
     pthread_mutex_destroy(&mutex);
+    cl_sin_destroy();
+    cl_mrg_destroy();
     return 0;
 }
 
-void createProgram(char *fileName) {
-
+void createProgram_sin(char *fileName) {
     FILE *program_handle;
     program_handle = fopen(fileName, "r");
     if (program_handle == NULL) {
@@ -468,67 +503,42 @@ void createProgram(char *fileName) {
         exit(1);
     }
     fseek(program_handle, 0, SEEK_END);
-    source_size = ftell(program_handle);
+    source_size_sin = ftell(program_handle);
     rewind(program_handle);
-    source_str = (char *) malloc(source_size + 1);
-    source_str[source_size] = '\0';
-    fread(source_str, sizeof(char), source_size, program_handle);
+    source_str_sin = (char *) malloc(source_size_sin + 1);
+    source_str_sin[source_size_sin] = '\0';
+    fread(source_str_sin, sizeof(char), source_size_sin, program_handle);
+    fclose(program_handle);
+}
+void createProgram_mrg(char *fileName) {
+    FILE *program_handle;
+    program_handle = fopen(fileName, "r");
+    if (program_handle == NULL) {
+        perror("Couldn't find the program file");
+        exit(1);
+    }
+    fseek(program_handle, 0, SEEK_END);
+    source_size_mrg = ftell(program_handle);
+    rewind(program_handle);
+    source_str_mrg = (char *) malloc(source_size_mrg + 1);
+    source_str_mrg[source_size_mrg] = '\0';
+    fread(source_str_mrg, sizeof(char), source_size_mrg, program_handle);
     fclose(program_handle);
 }
 
 void mergecl(float *M1, float *M2, int M2_size) {
-    cl_platform_id platform;
-    cl_int ret = clGetPlatformIDs(1, &platform, &platformCount);
-    //printf("clGetPlatformIDs ret %d\n", ret);
-//    printf("number platforms %d\n", platformCount);
 
-    ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, &uNumCPU);
-//    printf("clGetDeviceIDs ret %d\n", ret);
+    ret = clEnqueueWriteBuffer(queue_mrg, M1_clmem_mrg, CL_TRUE, 0, M2_size * sizeof(float), M1, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(queue_mrg, M2_clmem_mrg, CL_TRUE, 0, M2_size * sizeof(float), M2, 0, NULL, NULL);
 
-    cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &ret);
-//    printf("clCreateContext ret %d\n", ret);
-
-    cl_command_queue queue = clCreateCommandQueue(context, device, 0, &ret);
-//    printf("clCreateCommandQueue ret %d\n", ret);
-
-    createProgram("stage_merge.cl");
-    cl_program program = clCreateProgramWithSource(context, 1, (const char **) &source_str,
-                                                   (const size_t *) &source_size, &ret);
-//    printf("clCreateProgramWithSource ret %d\n", ret);
-
-    ret = clBuildProgram(program, 1, &device, "-cl-std=CL1.2", NULL, NULL);
-//    printf("clBuildProgram ret %d\n", ret);
-    if (ret == CL_BUILD_PROGRAM_FAILURE) {
-        size_t log_size;
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-        char *log = (char *) malloc(log_size);
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
-        printf("%s\n", log);
-    }
-
-    cl_kernel kernel = clCreateKernel(program, "merge", &ret);
-//    printf("clCreateKernel ret %d\n", ret);
-
-    cl_mem M1_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY, M2_size * sizeof(float), NULL, &ret);
-//    printf("clCreateBuffer M1 ret %d\n", ret);
-    cl_mem M2_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY, M2_size * sizeof(float), NULL, &ret);
-//    printf("clCreateBuffer M2 ret %d\n", ret);
-    cl_mem M2_ret_clmem = clCreateBuffer(context, CL_MEM_WRITE_ONLY, M2_size * sizeof(float), NULL, &ret);
-//    printf("clCreateBuffer M2_ret ret %d\n", ret);
-
-    ret = clEnqueueWriteBuffer(queue, M1_clmem, CL_TRUE, 0, M2_size * sizeof(float), M1, 0, NULL, NULL);
-//    printf("clEnqueueWriteBuffer M1_clmem ret %d\n", ret);
-    ret = clEnqueueWriteBuffer(queue, M2_clmem, CL_TRUE, 0, M2_size * sizeof(float), M2, 0, NULL, NULL);
-//    printf("clEnqueueWriteBuffer M2_clmem ret %d\n", ret);
-
-    clSetKernelArg(kernel, 0, sizeof(M1_clmem), (void *) &M1_clmem);
-    clSetKernelArg(kernel, 1, sizeof(M2_clmem), (void *) &M2_clmem);
-    clSetKernelArg(kernel, 2, sizeof(M2_ret_clmem), (void *) &M2_ret_clmem);
+    clSetKernelArg(kernel_mrg, 0, sizeof(M1_clmem_mrg), (void *) &M1_clmem_mrg);
+    clSetKernelArg(kernel_mrg, 1, sizeof(M2_clmem_mrg), (void *) &M2_clmem_mrg);
+    clSetKernelArg(kernel_mrg, 2, sizeof(M2_ret_clmem_mrg), (void *) &M2_ret_clmem_mrg);
 
     size_t uGlobalWorkSize = M2_size;
     clEnqueueNDRangeKernel(
-            queue,
-            kernel,
+            queue_mrg,
+            kernel_mrg,
             1,
             NULL,
             &uGlobalWorkSize,
@@ -536,81 +546,25 @@ void mergecl(float *M1, float *M2, int M2_size) {
             0,
             NULL,
             NULL);
-    clFinish(queue);
-
-    cl_float *puData = (cl_float *) clEnqueueMapBuffer(
-            queue,
-            M2_ret_clmem,
-            CL_TRUE,
-            CL_MAP_READ,
-            0,
-            M2_size * sizeof(cl_float),
-            0,
-            NULL,
-            NULL,
-            NULL);
+    clFinish(queue_mrg);
 
     for (int i = 0; i < M2_size; ++i)
-        M2[i] = puData[i];
+        M2[i] = puData_mrg[i];
 
-    clEnqueueUnmapMemObject(queue, M2_ret_clmem, puData, 0, NULL, NULL);
-    clReleaseContext(context);
-    clReleaseCommandQueue(queue);
-    clReleaseProgram(program);
-    free(source_str);
-    clReleaseKernel(kernel);
 }
 
 float sum_sin(float *M2, int M2_size, float min_non_zero) {
-    cl_platform_id platform;
-    cl_int ret = clGetPlatformIDs(1, &platform, &platformCount);
-    printf("clGetPlatformIDs ret %d\n", ret);
-    printf("number platforms %d\n", platformCount);
+    ret = clEnqueueWriteBuffer(queue_sin, M2_clmem_sin, CL_TRUE, 0, M2_size * sizeof(float), M2, 0, NULL, NULL);
 
-    ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, &uNumCPU);
-    printf("clGetDeviceIDs ret %d\n", ret);
-
-    cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &ret);
-    printf("clCreateContext ret %d\n", ret);
-
-    cl_command_queue queue = clCreateCommandQueue(context, device, 0, &ret);
-    printf("clCreateCommandQueue ret %d\n", ret);
-
-    createProgram("sum_sin.cl");
-    cl_program program = clCreateProgramWithSource(context, 1, (const char **) &source_str,
-                                                   (const size_t *) &source_size, &ret);
-    printf("clCreateProgramWithSource ret %d\n", ret);
-
-    ret = clBuildProgram(program, 1, &device, "-cl-std=CL1.2", NULL, NULL);
-    printf("clBuildProgram ret %d\n", ret);
-    if (ret == CL_BUILD_PROGRAM_FAILURE) {
-        size_t log_size;
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-        char *log = (char *) malloc(log_size);
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
-        printf("%s\n", log);
-    }
-
-    cl_kernel kernel = clCreateKernel(program, "sum_sin", &ret);
-    printf("clCreateKernel ret %d\n", ret);
-
-    cl_mem M2_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY, M2_size * sizeof(float), NULL, &ret);
-    printf("clCreateBuffer M2 ret %d\n", ret);
-    cl_mem M2_ret_clmem = clCreateBuffer(context, CL_MEM_WRITE_ONLY, M2_size * sizeof(float), NULL, &ret);
-    printf("clCreateBuffer M2_ret ret %d\n", ret);
-
-    ret = clEnqueueWriteBuffer(queue, M2_clmem, CL_TRUE, 0, M2_size * sizeof(float), M2, 0, NULL, NULL);
-    printf("clEnqueueWriteBuffer M2_clmem ret %d\n", ret);
-
-    clSetKernelArg(kernel, 0, sizeof(float), (void *) &min_non_zero);
-    clSetKernelArg(kernel, 1, sizeof(M2_clmem), (void *) &M2_clmem);
-    clSetKernelArg(kernel, 2, sizeof(M2_ret_clmem), (void *) &M2_ret_clmem);
+    clSetKernelArg(kernel_sin, 0, sizeof(float), (void *) &min_non_zero);
+    clSetKernelArg(kernel_sin, 1, sizeof(M2_clmem_sin), (void *) &M2_clmem_sin);
+    clSetKernelArg(kernel_sin, 2, sizeof(M2_ret_clmem_sin), (void *) &M2_ret_clmem_sin);
 
     size_t uGlobalWorkSize = M2_size;
     size_t uLocalWorkSize = num_cores;
     clEnqueueNDRangeKernel(
-            queue,
-            kernel,
+            queue_sin,
+            kernel_sin,
             1,
             NULL,
             &uGlobalWorkSize,
@@ -618,11 +572,46 @@ float sum_sin(float *M2, int M2_size, float min_non_zero) {
             0,
             NULL,
             NULL);
-    clFinish(queue);
+    clFinish(queue_sin);
 
-    cl_float *puData = (cl_float *) clEnqueueMapBuffer(
-            queue,
-            M2_ret_clmem,
+    float X = 0;
+    for (int i = 0; i < M2_size / num_cores; ++i)
+        X += puData_sin[i];
+    return X;
+}
+
+void cl_sin_init(int M2_size) {
+    cl_platform_id platform;
+    cl_int ret = clGetPlatformIDs(1, &platform, &platformCount);
+
+    ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, &uNumCPU);
+
+    context_sin = clCreateContext(NULL, 1, &device, NULL, NULL, &ret);
+
+    queue_sin = clCreateCommandQueue(context_sin, device, 0, &ret);
+
+    createProgram_sin("sum_sin.cl");
+    program_sin = clCreateProgramWithSource(context_sin, 1, (const char **) &source_str_sin,
+                                        (const size_t *) &source_size_sin, &ret);
+
+    ret = clBuildProgram(program_sin, 1, &device, "-cl-std=CL1.2", NULL, NULL);
+    printf("clBuildProgram ret %d\n", ret);
+    if (ret == CL_BUILD_PROGRAM_FAILURE) {
+        size_t log_size;
+        clGetProgramBuildInfo(program_sin, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+        char *log = (char *) malloc(log_size);
+        clGetProgramBuildInfo(program_sin, device, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+        printf("%s\n", log);
+    }
+
+    kernel_sin = clCreateKernel(program_sin, "sum_sin", &ret);
+
+    M2_clmem_sin = clCreateBuffer(context_sin, CL_MEM_READ_ONLY, M2_size * sizeof(float), NULL, &ret);
+    M2_ret_clmem_sin = clCreateBuffer(context_sin, CL_MEM_WRITE_ONLY, M2_size * sizeof(float), NULL, &ret);
+
+    puData_sin = (cl_float *) clEnqueueMapBuffer(
+            queue_sin,
+            M2_ret_clmem_sin,
             CL_TRUE,
             CL_MAP_READ,
             0,
@@ -631,17 +620,64 @@ float sum_sin(float *M2, int M2_size, float min_non_zero) {
             NULL,
             NULL,
             NULL);
+}
 
-    float X = 0;
-    for (int i = 0; i < M2_size / num_cores; ++i)
-        //printf("%f \n", puData[i]);
-        X += puData[i];
+void cl_mrg_init(int M2_size) {
+    cl_platform_id platform;
+    cl_int ret = clGetPlatformIDs(1, &platform, &platformCount);
 
-    clEnqueueUnmapMemObject(queue, M2_ret_clmem, puData, 0, NULL, NULL);
-    clReleaseContext(context);
-    clReleaseCommandQueue(queue);
-    clReleaseProgram(program);
-    free(source_str);
-    clReleaseKernel(kernel);
-    return X;
+    ret = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 1, &device, &uNumCPU);
+
+    context_mrg = clCreateContext(NULL, 1, &device, NULL, NULL, &ret);
+
+    queue_mrg = clCreateCommandQueue(context_mrg, device, 0, &ret);
+
+    createProgram_mrg("stage_merge.cl");
+    program_mrg = clCreateProgramWithSource(context_mrg, 1, (const char **) &source_str_mrg,
+                                        (const size_t *) &source_size_mrg, &ret);
+
+    ret = clBuildProgram(program_mrg, 1, &device, "-cl-std=CL1.2", NULL, NULL);
+    if (ret == CL_BUILD_PROGRAM_FAILURE) {
+        size_t log_size;
+        clGetProgramBuildInfo(program_mrg, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+        char *log = (char *) malloc(log_size);
+        clGetProgramBuildInfo(program_mrg, device, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+        printf("%s\n", log);
+    }
+
+    kernel_mrg = clCreateKernel(program_mrg, "merge", &ret);
+
+    M1_clmem_mrg = clCreateBuffer(context_mrg, CL_MEM_READ_ONLY, M2_size * sizeof(float), NULL, &ret);
+    M2_clmem_mrg = clCreateBuffer(context_mrg, CL_MEM_READ_ONLY, M2_size * sizeof(float), NULL, &ret);
+    M2_ret_clmem_mrg = clCreateBuffer(context_mrg, CL_MEM_WRITE_ONLY, M2_size * sizeof(float), NULL, &ret);
+
+    puData_mrg = (cl_float *) clEnqueueMapBuffer(
+            queue_mrg,
+            M2_ret_clmem_mrg,
+            CL_TRUE,
+            CL_MAP_READ,
+            0,
+            M2_size * sizeof(cl_float),
+            0,
+            NULL,
+            NULL,
+            NULL);
+}
+
+void cl_sin_destroy() {
+    clEnqueueUnmapMemObject(queue_sin, M2_ret_clmem_sin, puData_sin, 0, NULL, NULL);
+    clReleaseContext(context_sin);
+    clReleaseCommandQueue(queue_sin);
+    clReleaseProgram(program_sin);
+    free(source_str_sin);
+    clReleaseKernel(kernel_sin);
+}
+
+void cl_mrg_destroy() {
+    clEnqueueUnmapMemObject(queue_mrg, M2_ret_clmem_mrg, puData_mrg, 0, NULL, NULL);
+    clReleaseContext(context_mrg);
+    clReleaseCommandQueue(queue_mrg);
+    clReleaseProgram(program_mrg);
+    free(source_str_mrg);
+    clReleaseKernel(kernel_mrg);
 }
