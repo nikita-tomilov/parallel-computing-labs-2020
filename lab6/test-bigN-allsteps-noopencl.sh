b2da31a7e077fa6abfc29997825ea022c3575ee7
Nikita Tomilov <programmer174@icloud.com>
2020-12-21 00:34:49 +0300

    lab6: upload results for bigN

---
+++ lab6/test-bigN-allsteps-noopencl.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+rm -rf logs-*
+
+run() {
+  M=$1
+  for N in `echo 1000 5000 10000 $(seq 15000 15000 450000)`; do
+    ./lab6 $N $M 200 --noopencl | grep ':' | tee -a logs-$M.txt
+  done
+}
+
+results_allsteps_total() {
+  for M in logs-1.txt logs-2.txt logs-3.txt logs-4.txt logs-5.txt logs-6.txt logs-12.txt; do
+    grep "Milliseconds" < "$M" > "$M"'a'
+    sed -i s/'. Milliseconds passed: '/','/g *.txta
+    sed -i s/'N=[0-9]*'/''/g *.txta
+  done
+  pr -t -m logs-1.txta logs-2.txta logs-3.txta logs-4.txta logs-5.txta logs-6.txta logs-12.txta | tee results-noopencl-allsteps-total.csv
+  rm -rf *.txta
+}
+
+results_step() {
+  STEP=$1
+  for M in logs-1.txt logs-2.txt logs-3.txt logs-4.txt logs-5.txt logs-6.txt logs-12.txt; do
+    grep "$STEP" < "$M" > "$M"'a'
+    sed -i s/'[a-zA-Z ]*:'/','/g *.txta
+  done
+  pr -t -s" " -w 120 -m logs-1.txta logs-2.txta logs-3.txta logs-4.txta logs-5.txta logs-6.txta logs-12.txta | tee 'results-noopencl-allsteps-'"$STEP"'.csv'
+  rm -rf *.txta
+}
+
+for M in $(seq 1 6); do
+  echo "Threads count:" $M
+  run $M
+done
+
+M=12
+echo "Threads count:" $M
+run $M
+
+results_allsteps_total
+results_step Generate
+results_step Map
+results_step 'Prof map'
+results_step Merge
+results_step 'Prof merge'
+results_step Sort
+results_step Reduce
+rm -rf logs-*
+
+mkdir -p "results_bigN_noopencl"
+mv results-*.csv ./results_bigN_noopencl/
--- lab6/test-trust10-allsteps-withopencl.sh
+++ lab6/test-trust10-allsteps-withopencl.sh
@@ -6,21 +6,21 @@ run() {
   M=$1
   for N in $(seq 1200 3380 35000); do
     for K in $(seq 1 10); do
-      ./lab6 $N $M 200 --noopencl | grep ':' | tee -a logs-$M-$K.txt
+      ./lab6 $N $M 200 | grep ':' | tee -a logs-$M-$K.txt
       sleep 1
     done
   done
 }
 
 results_allsteps_total() {
-  for M in $(echo 0 1 2 3 4 5 6 12); do
+  for M in $(echo 1 2 3 4 5 6 12); do
     for K in $(seq 1 10); do
       FILENAME=logs-$M-$K.txt
       grep "Milliseconds" < "$FILENAME" > "$FILENAME"'a'
       sed -i s/'. Milliseconds passed: '/','/g *.txta
       sed -i s/'N=[0-9]*'/''/g *.txta
     done
-    pr -W 256 -t -m *.txta | tee results-noopencl-allsteps-threads-$M.csv
+    pr -W 256 -t -m *.txta | tee results-opencl-allsteps-threads-$M.csv
     rm -rf *.txta
   done
 }
@@ -28,13 +28,13 @@ results_allsteps_total() {
 results_step() {
   STEP=$1
 
-  for M in $(echo 0 1 2 3 4 5 6 12); do
+  for M in $(echo 1 2 3 4 5 6 12); do
     for K in $(seq 1 10); do
       FILENAME=logs-$M-$K.txt
       grep "$STEP" < "$FILENAME" > "$FILENAME"'a'
       sed -i s/'[a-zA-Z ]*:'/','/g *.txta
     done
-    pr -W 256 -t -m *.txta | tee results-noopencl-step-"$STEP"-threads-$M.csv
+    pr -W 256 -t -m *.txta | tee results-opencl-step-"$STEP"-threads-$M.csv
     rm -rf *.txta
   done
 }
@@ -48,17 +48,15 @@ M=12
 echo "Threads count:" $M
 run $M
 
-M=0
-echo "Threads count:" $M
-run $M
-
 results_allsteps_total
 results_step Generate
 results_step Map
+results_step 'Prof map'
 results_step Merge
+results_step 'Prof merge'
 results_step Sort
 results_step Reduce
 rm -rf logs-*
 
-mkdir -p "results_noopencl"
-mv results-*.csv ./results_noopencl/
+mkdir -p "results_trust10_opencl"
+mv results-*.csv ./results_trust10_opencl/
